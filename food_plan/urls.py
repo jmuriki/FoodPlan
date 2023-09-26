a083d208e20091d5c1bb3cc81c9ea63d160b0337
jmuriki <jmuriki@mail.ru>
2023-09-26 20:31:47 +0530

    Connect pages

--- food_plan/urls.py
+++ food_plan/urls.py
@@ -22,6 +22,14 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="main_page"),
+    path('auth/', views.auth, name="auth"),
+    path('card', views.card, name="card"),
+    path('lk', views.lk, name="lk"),
+    path('order', views.order, name="order"),
+    path('registration', views.registration, name="registration"),
+    path('login', views.login, name="login"),
+    path('logout', views.logout, name="logout"),
+    path('pay', views.pay, name="pay"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
--- food_plan/views.py
+++ food_plan/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
 	return render(request, 'index.html')
+
+
+def auth(request):
+	return render(request, 'auth.html')
+
+
+def card(request):
+	return render(request, 'card.html')
+
+
+def lk(request):
+	return render(request, 'lk.html')
+
+
+def order(request):
+	return render(request, 'order.html')
+
+
+def registration(request):
+	return render(request, 'registration.html')
+
+
+def login(request):
+	return redirect('lk')
+
+
+def logout(request):
+	return redirect('main_page')
+
+
+def pay(request):
+	return render(request, 'pay.html')
+
